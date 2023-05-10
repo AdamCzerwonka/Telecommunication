37d714156ad5d17075d20860f34271b122e5130a
Marcel Badek <kriollik@gmail.com>
2023-05-10 22:30:13 +0200

    feat: init

---
+++ ConventerAC/ConventerAC.Core/ConventerAC.cs
@@ -0,0 +1,45 @@
+﻿using System.Media;
+using NAudio.Wave;
+
+namespace ConventerAC.Core;
+
+public class ConventerAC
+{
+    private WaveInEvent waveSource { get; set; }
+    private WaveFileWriter waveFile { get; set; }
+
+    public void StartRecording(int sampling, int quantization, string filePath)
+    {
+        waveSource = new WaveInEvent();
+        waveSource.WaveFormat = new WaveFormat(sampling, quantization, 2);
+
+        waveSource.DataAvailable += OnDataAvailable;
+        waveSource.RecordingStopped += OnRecordingStopped;
+        
+        waveFile = new WaveFileWriter(filePath, waveSource.WaveFormat);
+        waveSource.StartRecording();
+    }
+
+    private void OnDataAvailable(object? sender, WaveInEventArgs e)
+    {
+        waveFile.Write(e.Buffer, 0, e.BytesRecorded);
+        waveFile.Flush();
+    }
+
+    private void OnRecordingStopped(object? sender, StoppedEventArgs e)
+    {
+        waveSource.Dispose();
+        waveFile.Dispose();
+    }
+
+    public void StopRecording()
+    {
+        waveSource.StopRecording();
+    }
+
+    public void PlaySound(string path)
+    {
+        var player = new SoundPlayer(path);
+        player.Play();
+    }
+}
---
+++ ConventerAC/ConventerAC.Desktop/MainViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Runtime.InteropServices.JavaScript;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+
+namespace ConventerAC.Desktop;
+
+public class MainViewModel : NotifyPropertyChanged
+{
+    private string _saveFilePath;
+    private Core.ConventerAC _conventerAc;
+    private string _recordButtonText = "Start recording";
+
+    public MainViewModel()
+    {
+        _conventerAc = new();
+        RecordCommand = new RelayCommand(Record);
+        ChooseSaveFileCommand = new RelayCommand(ChooseSaveFile);
+        PlaySoundCommand = new RelayCommand(PlaySound);
+    }
+
+    public string RecordButtonText
+    {
+        get => _recordButtonText;
+        set
+        {
+            if (value == _recordButtonText) return;
+            _recordButtonText = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Sampling { get; set; } = "48000";
+    public string Quantization { get; set; } = "8";
+
+    public string SaveFilePath
+    {
+        get => _saveFilePath;
+        set
+        {
+            if (value == _saveFilePath) return;
+            _saveFilePath = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ICommand RecordCommand { get; }
+    public ICommand ChooseSaveFileCommand { get; }
+    public ICommand PlaySoundCommand { get; }
+
+    private bool _isRecording { get; set; } = false;
+
+    private void ChooseSaveFile()
+    {
+        var fileDialog = new SaveFileDialog();
+        if (fileDialog.ShowDialog() == false)
+        {
+            return;
+        }
+
+        SaveFilePath = fileDialog.FileName;
+    }
+
+    private void Record()
+    {
+        if (!_isRecording)
+        {
+            _conventerAc.StartRecording(int.Parse(Sampling), int.Parse(Quantization), SaveFilePath);
+            _isRecording = true;
+            RecordButtonText = "Stop recording";
+        }
+        else
+        {
+            _conventerAc.StopRecording();
+            _isRecording = false;
+            RecordButtonText = "Start recording";
+        }
+    }
+
+    private void PlaySound()
+    {
+        _conventerAc.PlaySound(SaveFilePath);
+    }
+}
